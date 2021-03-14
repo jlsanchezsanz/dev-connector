c9450c62eb20b49f05baebce21a83f678dc92612
jlsanchezsanz <jlsanchezsanz@hotmail.com>
2021-03-14 10:07:02 +0100

    feat: delete profile & user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -144,4 +144,21 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+/*
+ * @route  DELETE api/profile
+ * @desc   Delete profile, user & posts
+ * @access Private
+ */
+router.delete('/', auth, async (req, res) => {
+  try {
+    await Profile.findOneAndRemove({ user: req.user.id });
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User deleted' });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
