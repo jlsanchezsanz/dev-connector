908b2c4c978bb85c8d77afdd11389bbac339edb6
jlsanchezsanz <jlsanchezsanz@hotmail.com>
2021-03-15 17:50:06 +0100

    feat: get and delete post routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -11,7 +11,6 @@ const User = require('../../models/User');
  * @desc   Create a post
  * @access Private
  */
-
 router.post(
   '/',
   [auth, [check('text', 'Text is required').not().isEmpty()]],
@@ -40,5 +39,76 @@ router.post(
     }
   }
 );
+
+/*
+ * @route  GET api/posts
+ * @desc   Get all posts
+ * @access Private
+ */
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+
+    res.json(posts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+/*
+ * @route  GET api/posts/:post_id
+ * @desc   Get post by id
+ * @access Private
+ */
+router.get('/:post_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    res.json(post);
+  } catch (error) {
+    console.error(error.message);
+
+    if (error.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    res.status(500).send('Server Error');
+  }
+});
+
+/*
+ * @route  DELETE api/posts/:post_id
+ * @desc   Delete a post
+ * @access Private
+ */
+router.delete('/:post_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    await post.remove();
+    res.json({ msg: 'Post removed' });
+  } catch (error) {
+    console.error(error.message);
+
+    if (error.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
