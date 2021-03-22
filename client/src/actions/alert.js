afba3b88859e9ba9a33e4b3fd21278cd695c5a3b
jlsanchezsanz <jlsanchezsanz@hotmail.com>
2021-03-22 12:28:10 +0100

    feat: alert component and action call

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,10 +1,12 @@
 import { SET_ALERT, REMOVE_ALERT } from './types';
-import uuid from 'uuid';
+import { v4 } from 'uuid';
 
-export const setAlert = (msg, alertType) => (dispatch) => {
-  const id = uuid.v4();
+export const setAlert = (msg, alertType, timeout = 5000) => (dispatch) => {
+  const id = v4();
   dispatch({
     type: SET_ALERT,
-    payoad: { msg, alertType, id },
+    payload: { msg, alertType, id },
   });
+
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
 };
