473c0d3bd8a10401f1fa06e9d7e7c7408c276ab8
jlsanchezsanz <jlsanchezsanz@hotmail.com>
2021-03-22 17:14:11 +0100

    feat: auth reducer and register action

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,9 +2,10 @@ import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -22,7 +23,7 @@ const Register = ({ setAlert }) => {
     if (password !== password2) {
       setAlert('Passwords do not match', 'danger');
     } else {
-      console.log('SUCCESS');
+      register({ name, email, password });
     }
   };
 
@@ -66,6 +67,7 @@ const Register = ({ setAlert }) => {
               minLength="6"
               value={password}
               onChange={(e) => onChange(e)}
+              required
             />
           </div>
           <div className="form-group">
@@ -76,6 +78,7 @@ const Register = ({ setAlert }) => {
               minLength="6"
               value={password2}
               onChange={(e) => onChange(e)}
+              required
             />
           </div>
           <input type="submit" className="btn btn-primary" value="Register" />
@@ -90,6 +93,7 @@ const Register = ({ setAlert }) => {
 
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
 };
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
