1ca166230fdf40cb59fbed143c2edbeeb22a0de7
jlsanchezsanz <jlsanchezsanz@hotmail.com>
2021-03-24 22:11:08 +0100

    feat: delete education, experiences and account

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 const { check, validationResult } = require('express-validator');
 
 /*
@@ -153,6 +154,7 @@ router.get('/user/:user_id', async (req, res) => {
  */
 router.delete('/', auth, async (req, res) => {
   try {
+    await Post.deleteMany({ user: req.user.id });
     await Profile.findOneAndRemove({ user: req.user.id });
     await User.findOneAndRemove({ _id: req.user.id });
 
